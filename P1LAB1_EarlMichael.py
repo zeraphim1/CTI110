78d81b1897d418f394124e3f392ca29be71511ab
zeraphim1 <mtearl01@gmail.com>
2025-02-24 14:46:39 -0500

    Add files via upload
    first submission

---
+++ P1LAB1_EarlMichael.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " +  last_name
+print("Hello,", full_name + "!", "Welcome to CTI-110")
+
